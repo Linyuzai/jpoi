ea4ac2c5edb8057c904e896dc3b65f1ef19261c4
thz <t88510930>
2019-09-29 14:13:13 +0800

    完善图片写入，当前支持jpeg和png

--- src/main/java/com/github/linyuzai/jpoi/excel/write/setter/SupportValueSetter.java
+++ src/main/java/com/github/linyuzai/jpoi/excel/write/setter/SupportValueSetter.java
@@ -11,10 +11,7 @@ import com.github.linyuzai.jpoi.support.SupportValue;
 import org.apache.poi.ss.usermodel.*;
 
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -84,18 +81,38 @@ public class SupportValueSetter extends PoiValueSetter {
     private void createPicture(SupportPicture value, ClientAnchor anchor, Workbook workbook, Drawing<?> drawing) {
         int type = value.getType();
         if (value instanceof ByteArrayPicture) {
+            if (type < Workbook.PICTURE_TYPE_EMF || type > Workbook.PICTURE_TYPE_DIB) {
+                type = Workbook.PICTURE_TYPE_JPEG;
+            }
             drawing.createPicture(anchor, workbook.addPicture(((ByteArrayPicture) value).getBytes(), type));
         } else if (value instanceof BufferedImagePicture) {
-            ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
             try {
                 String format = value.getFormat();
                 if (format == null) {
-                    if (type == Workbook.PICTURE_TYPE_PNG) {
-                        format = "png";
-                    } else {
-                        format = "jpeg";
+                    switch (type) {
+                        /*case Workbook.PICTURE_TYPE_EMF:
+                            format = "emf";
+                            break;
+                        case Workbook.PICTURE_TYPE_WMF:
+                            format = "wmf";
+                            break;
+                        case Workbook.PICTURE_TYPE_PICT:
+                            format = "pict";
+                            break;*/
+                        case Workbook.PICTURE_TYPE_JPEG:
+                            format = "jpeg";
+                            break;
+                        case Workbook.PICTURE_TYPE_PNG:
+                            format = "png";
+                            break;
+                        /*case Workbook.PICTURE_TYPE_DIB:
+                            format = "dib";
+                            break;*/
+                        default:
+                            throw new IllegalArgumentException("Picture type[" + type + "] is unsupported");
                     }
                 }
+                ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
                 ImageIO.write(((BufferedImagePicture) value).getBufferedImage(), format, byteArrayOut);
                 createPicture(new ByteArrayPicture(value.getPadding(), value.getLocation(), value.getAnchorType(),
                         type, value.getFormat(), byteArrayOut.toByteArray()), anchor, workbook, drawing);
@@ -107,19 +124,63 @@ public class SupportValueSetter extends PoiValueSetter {
                 File file = ((FilePicture) value).getFile();
                 Path path = Paths.get(file.getAbsolutePath());
                 String mime = Files.probeContentType(path);
-                if (mime.equals("image/png")) {
-                    type = Workbook.PICTURE_TYPE_PNG;
+                switch (mime) {
+                    /*case "PICTURE_TYPE_EMF":
+                        type = Workbook.PICTURE_TYPE_EMF;
+                        break;
+                    case "PICTURE_TYPE_WMF":
+                        type = Workbook.PICTURE_TYPE_WMF;
+                        break;
+                    case "PICTURE_TYPE_PICT":
+                        type = Workbook.PICTURE_TYPE_PICT;
+                        break;*/
+                    case "image/jpeg":
+                    case "image/pjpeg":
+                        type = Workbook.PICTURE_TYPE_JPEG;
+                        break;
+                    case "image/png":
+                        type = Workbook.PICTURE_TYPE_PNG;
+                        break;
+                    /*case "PICTURE_TYPE_DIB":
+                        type = Workbook.PICTURE_TYPE_DIB;
+                        break;*/
+                    default:
+                        throw new IllegalArgumentException("Mime type[" + type + "] is unsupported");
                 }
-                BufferedImage bufferedImage = ImageIO.read(file);
+                /*BufferedImage bufferedImage = ImageIO.read(file);
                 createPicture(new BufferedImagePicture(value.getPadding(), value.getLocation(), value.getAnchorType(),
-                        type, value.getFormat(), bufferedImage), anchor, workbook, drawing);
+                        type, value.getFormat(), bufferedImage), anchor, workbook, drawing);*/
+                FileInputStream fis = new FileInputStream(file);
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                byte[] b = new byte[1024];
+                int n;
+                while ((n = fis.read(b)) != -1) {
+                    bos.write(b, 0, n);
+                }
+                fis.close();
+                bos.close();
+                createPicture(new ByteArrayPicture(value.getPadding(), value.getLocation(), value.getAnchorType(),
+                        type, value.getFormat(), bos.toByteArray()), anchor, workbook, drawing);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else if (value instanceof Base64Picture) {
-            byte[] bytes = Base64.getDecoder().decode(((Base64Picture) value).getBase64());
+            String format = value.getFormat();
+            String jpeg = "data:image/jpeg;base64,";
+            String png = "data:image/png;base64,";
+            String base64 = ((Base64Picture) value).getBase64();
+            if (base64.startsWith(jpeg)) {
+                base64 = base64.replace(jpeg, "");
+                type = Workbook.PICTURE_TYPE_JPEG;
+                format = "jpeg";
+            } else if (base64.startsWith(png)) {
+                base64 = base64.replace(png, "");
+                type = Workbook.PICTURE_TYPE_PNG;
+                format = "png";
+            }
+            byte[] bytes = Base64.getDecoder().decode(base64);
             createPicture(new ByteArrayPicture(value.getPadding(), value.getLocation(), value.getAnchorType(),
-                    type, value.getFormat(), bytes), anchor, workbook, drawing);
+                    type, format, bytes), anchor, workbook, drawing);
         }
     }
 }
